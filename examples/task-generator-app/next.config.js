1dc145957f6f0e7ff4f1d631effb4997b58a2530
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-05-23 13:45:26 +0200

    example(task-generator): add env variable validation

--- examples/task-generator-app/next.config.js
+++ examples/task-generator-app/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error('OPENAI_API_KEY environment variable is not defined')
+}
+
+if (!process.env.PEZZO_API_KEY) {
+  throw new Error('PEZZO_API_KEY environment variable is not defined')
+}
+
 module.exports = nextConfig
